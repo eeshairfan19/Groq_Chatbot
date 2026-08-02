21e286ffe1790a8fa295c0ae20404db865367213
eeshairfan19 <eeshairfan1907@gmail.com>
2026-08-03 01:23:36 +0500

    Replace Gemini integration with Groq API and enable AI response generation
    Integrated the Groq API using the official Groq Python SDK to power the Streamlit chatbot. Replaced the previous Gemini API implementation due to model availability and quota issues. Updated the application to authenticate using a Groq API key, switched to a supported Groq-hosted LLM, and implemented real-time AI response generation through the chat completions endpoint.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from groq import Groq
 
 st.set_page_config(
     page_title="AI Chatbot with Memory",
@@ -6,4 +7,32 @@ st.set_page_config(
 
 st.title("AI Chatbot with Memory")
 
-st.write("Welcome! Ask me anything.")
+client = Groq(
+    api_key=st.secrets["GROQ_API_Key"]
+)
+
+question = st.text_area(
+    "Ask a question:",
+    height=150,
+)
+
+if st.button("Generate Response"):
+
+    if question.strip():
+
+        with st.spinner("Thinking..."):
+
+            response = client.chat.completions.create(
+                model="llama-3.1-8b-instant",
+                messages=[
+        {
+            "role": "user",
+            "content": question,
+        }
+            ],
+            )
+
+        st.success(response.choices[0].message.content)
+
+    else:
+        st.warning("Please enter a question.")
